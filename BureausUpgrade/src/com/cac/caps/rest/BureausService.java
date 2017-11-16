e8b77577ad8aa13cb83a7cfcc51a0e099390de47
Hanumanth rao vallem <vallem.rao85@gmail.com>
2017-11-16 00:15:33 -0500

    Initial import

---
+++ BureausUpgrade/src/com/cac/caps/rest/BureausService.java
@@ -0,0 +1,41 @@
+package com.cac.caps.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class BureausService extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(applicationClass, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(applicationClass);
+    }
+
+    private static Class<BureausService> applicationClass = BureausService.class;
+}
+
+
+@RestController
+class GreetingController {
+
+    @RequestMapping(value = "/users", method = RequestMethod.POST)
+    @ResponseBody SampleObject hello(@RequestBody SampleObject user) {
+        return user;
+    }
+} 
---
+++ BureausUpgrade/src/com/cac/caps/rest/SampleObject.java
@@ -0,0 +1,29 @@
+package com.cac.caps.rest;
+
+public class SampleObject {
+	
+	private String firstName;
+	private String lastname;
+	private String middleName;
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getMiddleName() {
+		return middleName;
+	}
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+	
+	
+
+}
